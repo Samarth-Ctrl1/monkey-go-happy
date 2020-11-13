07e7df68dd56de94921b7cee2cfb402b683b031d
Samarth-Ctrl1 <74181618+Samarth-Ctrl1@users.noreply.github.com>
2020-11-13 15:42:22 +0530

    Add files via upload

---
+++ Project_16_(Happy_monkey_1)_copy_2020_11_13_10_08_45/sketch.js
@@ -0,0 +1,163 @@
+var monkey, monkey_running, banana, bananaImage, obstacle, obstacleImage;
+var foodGroup, obstacleGroup;
+var score;
+
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+function preload(){
+  monkey_running = loadAnimation("monkey_0.png", "monkey_1.png", "monkey_2.png", "monkey_3.png","monkey_4.png", "monkey_5.png", "monkey_6.png", "monkey_7.png", "monkey_8.png");
+  bananaImage=loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+  
+}
+function setup() {
+  //Canvas
+  createCanvas(400,400);
+  
+  //Groups
+  FoodGroup = createGroup();
+  obstacleGroup = createGroup();
+  TimeGroup = createGroup();
+  
+  //Monkey
+  monkey = createSprite(50, 250, 10, 10);
+  monkey.addAnimation("monkey",monkey_running);
+  monkey.scale = 0.1;
+  
+ //Ground
+  ground = createSprite(70, 350, 800, 10);
+  ground.velocityX = -4;
+  ground.x=ground.width/2;
+  
+  //score
+  score = 0;
+  survialTime = 0;
+  
+}
+
+//Draw
+function draw() {
+  
+  //Background
+  background (180);
+  
+   //displaying survialtime
+  stroke("black");
+    fill("black");
+      textSize(20);
+  
+  text("Survial Time:"+  survialTime, 100, 50);
+  
+  //displaying score
+  stroke("black");
+    fill("black");
+      textSize(20);
+  text("Score:"+  score, 300, 100);
+  
+ //Monkey
+  monkey.collide(ground);
+  //PLAY
+  if(gameState === PLAY){
+      monkey.changeAnimation("running", monkey_running);
+    
+    survialTime = Math.ceil(frameCount/frameRate());
+     
+    
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    
+    //jump when the space key is pressed
+    if(keyDown("space")) {
+        monkey.velocityY = -12;
+    }    
+    
+    if(FoodGroup.isTouching(monkey)) {
+      FoodGroup.destroyEach();
+      score = score+1;
+    }
+   
+  //Gravity
+  monkey.velocityY = monkey.velocityY + 0.8;
+  
+    
+  
+  
+  //groups lifetime
+  obstacleGroup.setLifetimeEach(-1);
+  
+  //Adding Functions
+  food();
+  obstacles();
+    
+    
+      
+    
+    
+    if(obstacleGroup.isTouching(monkey)){
+        
+        gameState = END;
+      
+    }
+  }
+  //END
+   if (gameState === END) {
+     obstacleGroup.destroyEach();
+    FoodGroup.destroyEach();
+     survialTime.visible = false;
+     
+
+     stroke("red");
+    fill("red");
+       textSize(30);
+  text("Game Over", 110, 200);
+     
+      stroke("black");
+    fill("black");
+       textSize(30);
+     text("Monkey is dead", 100, 240);
+   }
+ 
+  
+  
+ 
+
+  //draw Sprites
+  drawSprites();
+}
+
+//Banana
+function food() {
+  if (frameCount % 80 === 0) {
+    banana = createSprite(400,350,40,10);
+    banana.addImage(bananaImage);
+    banana.y = Math.round(random(120,200));
+    banana.scale = 0.1;
+    
+    banana.velocityX = -3;
+    banana.lifetime = 200;
+    
+    FoodGroup.add(banana);
+  }
+}
+
+//Obstacles
+function obstacles() {
+  if (frameCount % 300 === 0){
+    obstacle = createSprite(250,325,10,10);
+    obstacle.addImage(obstacleImage);
+    obstacle.velocityX = -3;
+    obstacle.lifetime = 200;
+    obstacle.scale = 0.1 ;
+     obstacleGroup.add(obstacle);
+  }
+
+}
+
+
+
+
+
+
